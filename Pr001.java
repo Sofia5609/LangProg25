8756de6ebe0e4a5dc8cf4e9b6f2f1bc1c180ec6f
komogorova@mail.ru <komogorova@mail.ru>
2025-10-22 11:00:45 +0700

    Обобщенный класс с двумя параметрами типов

--- Pr001.java
+++ Pr001.java
@@ -1,8 +1,8 @@
 class Gen<T> {
-	/Объявление переменной обобщённого типа
+	//Объявление переменной обобщённого типа
 	T ob;
 	//Определение конструктора, в качестве параметра указываем объект обобщенного типа
-	Gen (Т о) {
+	Gen (T o) {
 		ob = o;
 	}
 	//Метод,возввращающий объект 
@@ -14,23 +14,49 @@ class Gen<T> {
 		System.out.println("Тип Т: " + ob.getClass().getName());
 	}
 }
+class TwoGen<T,V>{
+	//Объявление переменной обобщённого типа
+	T ob1;
+	V ob2;
+	//Определение конструктора, в качестве параметра указываем объект обобщенного типа
+	TwoGen(T o1, V o2) {
+		ob1 = o1;
+		ob2 = o2;
+
+	}
+	//Метод,возввращающий объект
+	T getOb1() {
+		return ob1;
+	}
+	V getOb2() {
+		return ob2;
+	}
+	//Метод,получающий информацию об имени типа
+	void showTypes(){ 
+		System.out.println("Тип Т: " + ob1.getClass().getName());
+		System.out.println("Тип V: " + ob2.getClass().getName());
+	} 
+
+}
+
 class Pr001 {
 	public static void main(String[] args) {
+		Gen<String> strOb = new Gen<String>("Один обобщенный тип");
+		String str = strOb.getOb();
+		System.out.println("Значение обобщенного типа с одним параметром");
 		//Создаем ссылочную переменную на объект типа Integer
-		Gen<Integer> iOb;
+		TwoGen<Integer, String> tgOb;
 		//Присваиваем ссылку на новый объект
-		iOb = new Gen<Integer>(88);
+		tgOb = new TwoGen<Integer, String>(88, "Обобщения");
 		//Выводим информацию о типе объекта
-		iOb.showType();
+		tgOb.showTypes();
 		//Присваиваем целой переменной значение,хранящееся в объекте
-		int v = iOb.getOb();
+		int v = tgOb.getOb1();
 		System.out.println("Значение: " + v);
 
 		System.out.println();
 		//Создаем ссылочную переменную на объект типа String и присваиваем ссылку на новый объект
-		Gen<String>strOb = new Gen<String>("Строка обобщённого класса");
-		strOb.showType();
-		String str = strOb.getOb();
+		str = tgOb.getOb2();
 		System.out.println("Значение в переменной str: " + str);
 	}
-} 
+}
